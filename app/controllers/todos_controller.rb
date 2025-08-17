baa03e4ce88b0480193bad590063b276b7ece3ce
Nawamin Wongpakham <nawamin@MacBook-Air-khxngNawamin.local>
2025-08-17 16:16:40 +0700

    feat: Add todopage

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,11 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:edit, :update, :destroy]
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
   def index
     @todos = Todo.all.order(created_at: :desc)
+  end
+
+  def show 
   end
 
   def new
@@ -11,26 +14,37 @@ class TodosController < ApplicationController
 
   def create
     @todo = Todo.new(todo_params)
-    if @todo.save
-      redirect_to root_path, notice: "Todo created!"
-    else
-      render :new
+     respond_to do |format|
+      if @todo.save
+        format.turbo_stream # Renders create.turbo_stream.erb
+        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}_form", partial: "form", locals: { todo: @todo }) }
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
   end
 
   def edit; end
 
   def update
-    if @todo.update(todo_params)
-      redirect_to root_path, notice: "Todo updated!"
-    else
-      render :edit
+    respond_to do |format|
+      if @todo.update(todo_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}", partial: "todo", locals: { todo: @todo }) }
+        format.html { redirect_to root_path, notice: "Todo updated successfully." }
+      else
+        format.html { render :edit }
+      end
     end
   end
 
   def destroy
-    @todo.destroy
-    redirect_to root_path, notice: "Todo deleted!"
+   @todo.destroy!
+  
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@todo)}") }
+      format.html { redirect_to todos_path, status: :see_other, notice: "Todo was successfully destroyed." }
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +12,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "todos#index"
-  resources :todos, only: [:index, :new, :create, :edit, :update, :destroy]
 end
